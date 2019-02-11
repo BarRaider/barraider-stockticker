3da8f91df2a807d4d305b91972a2aa36fead869a
BarRaider <nospam+gh@blachshma.com>
2019-02-11 08:38:51 -0800

    Intial version

---
+++ barraider-stockticker/StockComm.cs
@@ -0,0 +1,93 @@
+﻿using BarRaider.SdTools;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarRaider.StockTicker
+{
+    internal class StockComm
+    {
+        private const string STOCK_URI_PREFIX = "https://api.iextrading.com/1.0/stock/";
+        private const string STOCK_BATCH_CHART_QUOTE = "market/batch";
+        private const string CHART_DAILY = "1m";
+        private const string CHART_MINUTE = "1d";
+        private const int DEFAULT_CHART_POINTS = 36;
+
+        public async Task<SymbolData> GetSymbol(string stockSymbol)
+        {
+            var kvp = new List<KeyValuePair<string, string>>();
+            kvp.Add(new KeyValuePair<string, string>("symbols", stockSymbol));
+            kvp.Add(new KeyValuePair<string, string>("types", "quote,chart"));
+            kvp.Add(new KeyValuePair<string, string>("range", "dynamic"));
+            //kvp.Add(new KeyValuePair<string, string>("chartLast", DEFAULT_CHART_POINTS.ToString()));
+            HttpResponseMessage response = await StockQuery(STOCK_BATCH_CHART_QUOTE, kvp);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string body = await response.Content.ReadAsStringAsync();
+                var obj = JObject.Parse(body);
+
+                // Invalid Stock Symbol
+                if (obj.Count == 0)
+                {
+                    Logger.Instance.LogMessage(TracingLevel.ERROR, $"GetSymbol invalid symbol: {stockSymbol}");
+                    return null;
+                }
+
+                var jp = obj.Properties().First();
+                StockQuote quote = jp.Value["quote"].ToObject<StockQuote>();
+
+                ChartBase[] chart;
+
+                if (jp.Value["chart"]["range"].ToString() == CHART_DAILY)
+                {
+                    chart = GetDailyChart(jp.Value["chart"]["data"]);
+                }
+                else
+                {
+                    chart = GetMinuteChart(jp.Value["chart"]["data"]);
+                }
+                return new SymbolData(quote.Symbol, quote, chart);
+            }
+            else
+            {
+                Logger.Instance.LogMessage(TracingLevel.ERROR, $"GetSymbol invalid response: {response.StatusCode}");
+            }
+            return null;
+        }
+
+        private DailyChart[] GetDailyChart(JToken data)
+        {
+            return data.ToObject<DailyChart[]>();
+        }
+
+        private MinuteChart[] GetMinuteChart(JToken data)
+        {
+            return data.ToObject<MinuteChart[]>();
+        }
+
+        private async Task<HttpResponseMessage> StockQuery(string uriPath, List<KeyValuePair<string, string>> optionalContent)
+        {
+            string queryParams = string.Empty;
+            var client = new HttpClient();
+            //client.Timeout = new TimeSpan(0, 0, 10);
+
+            if (optionalContent != null)
+            {
+                var query = System.Web.HttpUtility.ParseQueryString(string.Empty);
+                foreach (var kvp in optionalContent)
+                {
+                    query[kvp.Key] = kvp.Value;
+
+                }
+                queryParams = "?" + query.ToString();
+            }
+            return await client.GetAsync($"{STOCK_URI_PREFIX}{uriPath}{queryParams}");
+        }
+    }
+}
---
+++ barraider-stockticker/StockManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarRaider.StockTicker
+{
+    public class StockManager
+    {
+        private StockComm connection;
+        private static StockManager instance = null;
+        private static readonly object objLock = new object();
+
+        #region Constructors
+
+        public static StockManager Instance
+        {
+            get
+            {
+                if (instance != null)
+                {
+                    return instance;
+                }
+
+                lock (objLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new StockManager();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        private StockManager()
+        {
+            connection = new StockComm();
+        }
+
+        #endregion
+
+        public async Task<SymbolData> GetSymbol(string stockSymbol)
+        {
+            return await connection.GetSymbol(stockSymbol);
+        }
+
+    }
+}
